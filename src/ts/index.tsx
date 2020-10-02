b46ea939f88e985a9c167420416cc6423c86e8af
Hikaru Terazono (3c1u) <3c1u@vulpesgames.tokyo>
2020-10-02 23:17:48 +0900

    Add stub

---
+++ src/ts/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const App = (props: {}) => {
+    return (<>
+        <h2>
+            🌈🦀
+        </h2>
+    </>)
+}
+
+ReactDOM.render((
+    <App />
+), document.getElementById('app'))
